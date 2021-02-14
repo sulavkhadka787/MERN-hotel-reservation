40f32fd67d363bee3684590550564dca3f187b03
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-02-14 09:27:56 -0700

    register-login-page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const Register=()=>{
+const Login=()=>{
     return(
 
         <div className="bckground">
             <h1>Hotel Shangri-La</h1>
             <div className="in-bck">
-            <h2>Register your account</h2>
+            <h2>Please Login</h2>
                 <form action="/action_page.php" className="bg-img">
 
                     <label for="email"><b>Email</b></label>
-                    <input type="email" placeholder="Enter Email" name="email" required />
+                    <input type="text" placeholder="Enter Email" name="email" required />
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="psw" required />
 
                     <button type="submit" className="btn">Login</button>
                 </form>
@@ -21,4 +24,4 @@ const Register=()=>{
     )
 }
 
-export default Register;
+export default Login;
